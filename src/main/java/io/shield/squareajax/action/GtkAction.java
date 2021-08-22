43860bfdfcbcce4bb617c428c0d77d96527c74bb
Wu Yuanshou <wuyuanshou@protonmail.com>
2021-08-22 20:30:35 +0800

    增加更多的proto示例

--- src/main/java/io/shield/squareajax/action/GtkAction.java
+++ src/main/java/io/shield/squareajax/action/GtkAction.java
@@ -6,24 +6,30 @@ package io.shield.squareajax.action;
 import io.shield.squareajax.annotation.ZPath;
 import io.shield.squareajax.constant.MessageProtocal;
 import lombok.extern.slf4j.Slf4j;
-import github.phiysng.io.shield.SkyScreen;
+import github.phiysng.io.shield.Datas.IntData;
+import github.phiysng.io.shield.Datas.LongData;
+import github.phiysng.io.shield.SkyScreenOuterClass.SkyScreen;
+import github.phiysng.io.shield.SkyScreenOuterClass.ThunderSkyScreen;
 
 @Slf4j
 public class GtkAction {
-	
+
 	@ZPath(protocal = MessageProtocal.LOGIN)
-	public void login(long roleId) {
+	public void login(LongData longData) {
 		log.info("login operation end...");
 	}
-	
+
 	@ZPath(protocal = MessageProtocal.LOGOUT)
-	public void logout(long roleId) {
-		log.error("logout operation end..");
+	public void logout(LongData longData) {
+		log.error("logout operation end.. roleId = {}" , longData.getLong());
 	}
 
 	@ZPath(protocal = MessageProtocal.BUY)
-	public void buy(Long roleId){
-		SkyScreen.SkyScrren skyScrren = SkyScreen.SkyScrren.newBuilder().setPriority(5).setMsg("Hello Broadcast Message").build();
+	public void buy(LongData longData, IntData number) {
+		SkyScreen skyScrren = SkyScreen.newBuilder().setPriority(5).setMsg("Hello Broadcast Message").build();
+		ThunderSkyScreen thunderSkyScreen = ThunderSkyScreen.newBuilder().setSkyScreen(skyScrren).build();
 		log.error("{}", skyScrren);
+
+		log.debug("roleId : {} number : {}", longData.getLong(), number.getInt());
 	}
 }
