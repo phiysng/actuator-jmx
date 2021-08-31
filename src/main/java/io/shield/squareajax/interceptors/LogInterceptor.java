3e1a0f9bd4a60dbfe809068b4f549eaf30122f14
wyshou <wyshou@wooduan.com>
2021-08-31 20:59:05 +0800

    show what MDC is.

---
+++ src/main/java/io/shield/squareajax/interceptors/LogInterceptor.java
@@ -0,0 +1,23 @@
+package io.shield.squareajax.interceptors;
+
+import org.slf4j.MDC;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+public class LogInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        UUID uuid = UUID.randomUUID();
+        MDC.put("requestId" , String.valueOf(uuid));
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        MDC.clear();
+    }
+}
